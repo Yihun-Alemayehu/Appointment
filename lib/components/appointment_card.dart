e88da59a4ff85a562e7111823641d44566a685fb
Yihun Alemayehu <yankure01@gmail.com>
2024-01-04 13:57:11 +0300

    fdb-05

---
+++ lib/components/appointment_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppointmentCard extends StatelessWidget {
+  const AppointmentCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/screens/appointment_screen.dart
+++ lib/screens/appointment_screen.dart
@@ -1,3 +1,4 @@
+import 'package:doctor_appointment/utils/config.dart';
 import 'package:flutter/material.dart';
 
 class AppointmentScreen extends StatefulWidget {
@@ -7,7 +8,7 @@ class AppointmentScreen extends StatefulWidget {
   State<AppointmentScreen> createState() => _AppointmentScreenState();
 }
 
-enum FilterStatus {upcoming, complete, cancel}
+enum FilterStatus { upcoming, complete, cancel }
 
 class _AppointmentScreenState extends State<AppointmentScreen> {
   FilterStatus status = FilterStatus.upcoming;
@@ -17,47 +18,185 @@ class _AppointmentScreenState extends State<AppointmentScreen> {
       "doctor_name": "Dr. Robel yonas",
       "doctor_profile": "assets/doctor2.png",
       "category": "Dentist",
-      "status": "upcoming",
+      "status": FilterStatus.upcoming,
     },
     {
       "doctor_name": "Dr. zerihun yonas",
       "doctor_profile": "assets/doctor1.png",
       "category": "Dentist",
-      "status": "complete",
+      "status": FilterStatus.complete,
     },
     {
       "doctor_name": "Dr. Simon yonas",
       "doctor_profile": "assets/doctor2.png",
       "category": "Respiration",
-      "status": "complete",
+      "status": FilterStatus.complete,
     },
     {
       "doctor_name": "Dr. Kalkidan yonas",
       "doctor_profile": "assets/doctor1.png",
       "category": "Cardiology",
-      "status": "cancel",
+      "status": FilterStatus.cancel,
     },
-
   ];
   @override
   Widget build(BuildContext context) {
     List<dynamic> filteredSchedules = schedules.where((var schedule) {
-      switch(schedule['status']){
-        case 'upcoming':
-          schedule['status'] = FilterStatus.upcoming;
-          break;
-        case 'complete':
-          schedule['status'] = FilterStatus.complete;
-          break;
-        case 'cancel':
-          schedule['status'] = FilterStatus.cancel;
-          break;
-      }
-      return schedule['status'];
+      // switch (schedule['status']) {
+      //   case 'upcoming':
+      //     schedule['status'] = FilterStatus.upcoming;
+      //     break;
+      //   case 'complete':
+      //     schedule['status'] = FilterStatus.complete;
+      //     break;
+      //   case 'cancel':
+      //     schedule['status'] = FilterStatus.cancel;
+      //     break;
+      // }
+      return schedule['status'] == status;
     }).toList();
-    return SafeArea(
-      child: Column(
-        children: [],
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 219, 229, 237),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 40, top: 20, right: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                'Appointment Schedule',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Config.spaceSmall,
+              Stack(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        for (FilterStatus filterStatus in FilterStatus.values)
+                          Expanded(
+                            child: GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  if (filterStatus == FilterStatus.upcoming) {
+                                    status = FilterStatus.upcoming;
+                                    _alignment = Alignment.centerLeft;
+                                  } else if (filterStatus ==
+                                      FilterStatus.complete) {
+                                    status = FilterStatus.complete;
+                                    _alignment = Alignment.center;
+                                  } else if (filterStatus ==
+                                      FilterStatus.cancel) {
+                                    status = FilterStatus.cancel;
+                                    _alignment = Alignment.centerRight;
+                                  }
+                                });
+                              },
+                              child: Center(child: Text(filterStatus.name)),
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                  AnimatedAlign(
+                    alignment: _alignment,
+                    duration: const Duration(milliseconds: 200),
+                    child: Container(
+                      width: 100,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.blue[900],
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Center(
+                        child: Text(
+                          status.name,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Config.spaceSmall,
+              Expanded(
+                  child: ListView.builder(
+                itemCount: filteredSchedules.length,
+                itemBuilder: (context, index) {
+                  var _schedule = filteredSchedules[index];
+                  bool isLastElement = filteredSchedules.length + 1 == index;
+                  return Card(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(
+                        color: Colors.blue,
+                      ),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    margin: !isLastElement
+                        ? const EdgeInsets.only(bottom: 20)
+                        : EdgeInsets.zero,
+                    child: Padding(
+                      padding: EdgeInsets.all(15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Row(
+                            children: [
+                              CircleAvatar(
+                                // backgroundColor: Colors.blue,
+                                backgroundImage:
+                                    AssetImage(_schedule['doctor_profile'],),
+                              ),
+                              Config.spaceSmall,
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    _schedule['doctor_name'],
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w800,
+                                    ),
+                                  ),
+                                  // const SizedBox(
+                                  //   height: 3,
+                                  // ),
+                                  Text(
+                                    _schedule['category'],
+                                    style: const TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w800,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              )),
+            ],
+          ),
+        ),
       ),
     );
   }
