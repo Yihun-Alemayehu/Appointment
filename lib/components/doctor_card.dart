baf776174864781cd6ea2ef3d8b7844d1b6a4859
Yihun Alemayehu <yankure01@gmail.com>
2024-01-04 11:18:40 +0300

    fdb-03

--- lib/components/doctor_card.dart
+++ lib/components/doctor_card.dart
@@ -11,20 +11,71 @@ class _DoctorCardState extends State<DoctorCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 150,
-      width: 150,
+      height: 200,
+      width: 280,
       child: GestureDetector(
-        child: const Card(
+        child: Card(
           child: Row(
             children: [
-              Column(
-                children: [
-                  
-                ],
-              )
+              const Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Dr. Robel Yonas'),
+                      SizedBox(
+                        height: 3,
+                      ),
+                      Text('Medical specialist'),
+                      SizedBox(
+                        height: 3,
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.star,
+                            color: Color.fromARGB(255, 255, 147, 7),
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Color.fromARGB(255, 255, 147, 7),
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Color.fromARGB(255, 255, 147, 7),
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Color.fromARGB(255, 255, 147, 7),
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Color.fromARGB(255, 255, 147, 7),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text('Experience'),
+                      Text('8 years'),
+                      SizedBox(
+                        height: 3,
+                      ),
+                      Text('Patients'),
+                      Text('1.08k'),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Image.asset('assets/doctor2.png'),
+              ),
             ],
           ),
         ),
+        onTap: () {},
       ),
     );
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:doctor_appointment/components/doctor_card.dart';
 import 'package:doctor_appointment/utils/config.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -25,188 +26,204 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: const Color.fromARGB(255, 219, 229, 237),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: SafeArea(
-            child: Column(
-          children: [
-            Row(
-              children: [
-                const Text(
-                  'Find your Specialist',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 23,
+        child: SingleChildScrollView(
+          child: SafeArea(
+              child: Column(
+            children: [
+              Row(
+                children: [
+                  const Text(
+                    'Find your Specialist',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 23,
+                    ),
                   ),
-                ),
-                const Spacer(),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15),
-                  child: Icon(
-                    Icons.search_outlined,
-                    size: 30,
+                  const Spacer(),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: Icon(
+                      Icons.search_outlined,
+                      size: 30,
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: Image.asset(
-                    'assets/messenger.png',
-                    width: 20,
-                    height: 20,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Image.asset(
+                      'assets/messenger.png',
+                      width: 20,
+                      height: 20,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 15),
-              child: CarouselSlider(
-                options: CarouselOptions(height: 180.0),
-                items: [
-                  {
-                    "name": "Dr. Robel Yonas",
-                    "specialization": "Medicina Specialist",
-                    "image": "doctor1.png"
-                  },
-                  {
-                    "name": "Dr. Zerihun Yonas",
-                    "specialization": "Dentist",
-                    "image": "doctor2.png"
-                  },
-                  {
-                    "name": "Dr. Robel Yonas",
-                    "specialization": "Medicinal Specialist",
-                    "image": "doctor1.png"
-                  },
-                ].map((i) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Container(
-                        width: 300,
-                        child: Card(
-                          color: Colors.blueAccent.shade700,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              children: [
-                                Expanded(
-                                  child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceAround,
-                                    children: [
-                                      Text(
-                                        'Looking for your Desire Specialist Doctor ?',
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.w800,
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                child: CarouselSlider(
+                  options: CarouselOptions(height: 180.0),
+                  items: [
+                    {
+                      "name": "Dr. Robel Yonas",
+                      "specialization": "Medicina Specialist",
+                      "image": "doctor1.png"
+                    },
+                    {
+                      "name": "Dr. Zerihun Yonas",
+                      "specialization": "Dentist",
+                      "image": "doctor2.png"
+                    },
+                    {
+                      "name": "Dr. Robel Yonas",
+                      "specialization": "Medicinal Specialist",
+                      "image": "doctor1.png"
+                    },
+                  ].map((i) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Container(
+                          width: 300,
+                          child: Card(
+                            color: Colors.blueAccent.shade700,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceAround,
+                                      children: [
+                                        Text(
+                                          'Looking for your Desire Specialist Doctor ?',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w800,
+                                          ),
                                         ),
-                                      ),
-                                      Container(
-                                        child: Row(
-                                          children: [
-                                            Container(
-                                              color: Colors.green,
-                                              width: 3,
-                                              height: 50,
-                                            ),
-                                            SizedBox(
-                                              width: 3,
-                                            ),
-                                            Column(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: [
-                                                Text(
-                                                  i["name"]!,
-                                                  style: TextStyle(
-                                                    color: Colors.white,
-                                                    fontWeight: FontWeight.w800,
+                                        Container(
+                                          child: Row(
+                                            children: [
+                                              Container(
+                                                color: Colors.green,
+                                                width: 3,
+                                                height: 50,
+                                              ),
+                                              SizedBox(
+                                                width: 3,
+                                              ),
+                                              Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  Text(
+                                                    i["name"]!,
+                                                    style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontWeight:
+                                                          FontWeight.w800,
+                                                    ),
                                                   ),
-                                                ),
-                                                Text(
-                                                  i["specialization"]!,
-                                                  style: TextStyle(
-                                                    color: Colors.white,
-                                                    fontWeight: FontWeight.w800,
-                                                    fontSize: 13,
+                                                  Text(
+                                                    i["specialization"]!,
+                                                    style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontWeight:
+                                                          FontWeight.w800,
+                                                      fontSize: 13,
+                                                    ),
                                                   ),
-                                                ),
-                                              ],
-                                            ),
-                                          ],
-                                        ),
-                                      )
-                                    ],
+                                                ],
+                                              ),
+                                            ],
+                                          ),
+                                        )
+                                      ],
+                                    ),
                                   ),
-                                ),
-                                Expanded(
-                                  child: Image.asset('assets/${i["image"]!}'),
-                                ),
-                              ],
+                                  Expanded(
+                                    child: Image.asset('assets/${i["image"]!}'),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                      );
-                    },
-                  );
-                }).toList(),
+                        );
+                      },
+                    );
+                  }).toList(),
+                ),
               ),
-            ),
-            Config.spaceSmall,
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Text(
-                    'Categories',
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                  Spacer(),
-                  Text('See All'),
-                ],
-              ),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: List<Widget>.generate(
-                  medCat.length,
-                  (index) => Container(
-                    width: 120,
-                    height: 120,
-                    child: (Card(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          FaIcon(medCat[index]['icon']),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          Text(medCat[index]['category'])
-                        ],
+              Config.spaceSmall,
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Categories',
+                      style: TextStyle(
+                        fontSize: 20,
                       ),
-                    )),
+                    ),
+                    Spacer(),
+                    Text('See All'),
+                  ],
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List<Widget>.generate(
+                    medCat.length,
+                    (index) => Container(
+                      width: 120,
+                      height: 120,
+                      child: (Card(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            FaIcon(medCat[index]['icon']),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text(medCat[index]['category'])
+                          ],
+                        ),
+                      )),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Config.spaceSmall,
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Text(
-                    'Available Doctor',
-                    style: TextStyle(
-                      fontSize: 20,
+              Config.spaceSmall,
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Available Doctor',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    Spacer(),
+                    Text('See All'),
+                  ],
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List<Widget>.generate(
+                    6,
+                    (index) => Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: (DoctorCard()),
                     ),
                   ),
-                  Spacer(),
-                  Text('See All'),
-                ],
-              ),
-            ),
-          ],
-        )),
+                ),
+              )
+            ],
+          )),
+        ),
       ),
     );
   }
