a219ff103188de99d6f3dcf70b2bc837a25a6150
Yihun Alemayehu <yankure01@gmail.com>
2024-01-04 20:27:58 +0300

    fdb-10

--- lib/screens/doctor_details.dart
+++ lib/screens/doctor_details.dart
@@ -14,11 +14,68 @@ class _DoctorDetailState extends State<DoctorDetail> {
   bool isFav = false;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Config.bgColor,
       appBar: CustomAppBar(
         appTitle: 'Doctor Details',
-        icon: FaIcon(Icons.arrow_back_ios),
+        icon: const FaIcon(Icons.arrow_back_ios),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: IconButton(
+              onPressed: () {
+                setState(() {
+                  isFav = !isFav;
+                });
+              },
+              icon: FaIcon(
+                isFav ? Icons.favorite_rounded : Icons.favorite_outline,
+                color: Colors.red,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: const SafeArea(
+        child: Column(
+          children: [
+            AboutDoctor(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AboutDoctor extends StatelessWidget {
+  const AboutDoctor({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: const Column(
+        children: [
+          CircleAvatar(
+            radius: 65,
+            backgroundImage: AssetImage('assets/doctor2.png'),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Text(
+            'Dr. Robel Yonas',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SizedBox(child: Text('HU (Hawassa University) , St. Paul Medical University'))
+        ],
       ),
     );
   }
