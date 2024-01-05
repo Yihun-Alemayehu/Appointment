93ce4e5bd3b5ffc111b7f80ad9465bb6a7cfd77e
Yihun Alemayehu <yankure01@gmail.com>
2024-01-05 10:21:17 +0300

    fdb-11

--- lib/screens/doctor_details.dart
+++ lib/screens/doctor_details.dart
@@ -74,9 +74,76 @@ class AboutDoctor extends StatelessWidget {
           SizedBox(
             height: 20,
           ),
-          SizedBox(child: Text('HU (Hawassa University) , St. Paul Medical University'))
+          SizedBox(
+            child: Text(
+              'HU (Hawassa University) , St. Paul Medical University',
+              style: TextStyle(
+                fontSize: 15,
+                color: Colors.grey,
+              ),
+              softWrap: true,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            child: Text(
+              'St. Paul Medical University',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+                color: Colors.black,
+              ),
+              softWrap: true,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+
         ],
       ),
+    );
+  }
+}
+
+class DetailBody extends StatelessWidget {
+  const DetailBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      margin: const EdgeInsets.only(bottom: 20),
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Config.spaceSmall,
+
+        ],
+      ),
+    );
+  }
+}
+
+
+class DoctorInfo extends StatelessWidget {
+  const DoctorInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(child: 
+        Container(
+          decoration: BoxDecoration(
+            
+          ),
+        ))
+      ],
     );
   }
 }
